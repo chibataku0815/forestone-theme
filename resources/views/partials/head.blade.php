7afbb295ad90e697b47ca99e0a4de07ede0c5f2a
千葉工 <chibataku0815@gmail.com>
2017-06-18 01:27:30 +0900

    add ie_hack

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,4 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php(wp_head())
+  <!--[if lte IE 10 ]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/flexibility/2.0.1/flexibility.js" data-turbolinks-track="true"></script>
+  <![endif]-->
 </head>
