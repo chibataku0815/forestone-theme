2b5643c22ae7bbcdf15f777a937be8c663bff685
千葉工 <chibataku0815@gmail.com>
2017-06-18 21:26:05 +0900

    test

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,3 @@
 <footer class="mdl-color--black">
     @php(dynamic_sidebar('sidebar-footer'))
-      <div class="mdl-grid mdl-color-text--blue-grey-700 flex flex--center">
-        <em>Copyright Japan-sneaker.com, All Rights Reserved</em>
-      </div>
 </footer>
