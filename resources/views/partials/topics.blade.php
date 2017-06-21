4717132ca55819a6552a6febe8dcdadbaf5499fa
千葉工 <chibataku0815@gmail.com>
2017-06-21 21:15:48 +0900

    test

--- resources/views/partials/topics.blade.php
+++ resources/views/partials/topics.blade.php
@@ -1,7 +1,7 @@
 <?php
   use WP_Query;
-  $args = array('post_type' => 'topics', 'posts_per_page' => 1, 'post_status' => 'publish');
+  $args = array('category_name' => 'topics', 'posts_per_page' => 1);
   $featured = new WP_Query($args);
   while($featured->have_posts()) : $featured->the_post();
-  the_post_thumbnail();
+  the_title();
 endwhile; ?>
