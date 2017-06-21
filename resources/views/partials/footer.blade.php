ace80f7d815955334bf2270bcb7624cd7ade1d5f
千葉工 <chibataku0815@gmail.com>
2017-06-21 20:35:19 +0900

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,45 @@
-<footer class="mdl-color--black">
+<footer class="content-info">
+  <div class="container">
     @php(dynamic_sidebar('sidebar-footer'))
+    <footer class="mdl-mega-footer">
+      <div class="mdl-mega-footer__middle-section">
+
+        <div class="mdl-mega-footer__drop-down-section">
+          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+          <ul class="mdl-mega-footer__link-list">
+            <li><a href="#">運営者情報</a></li>
+            <li><a>問合せ</a></li>
+          </ul>
+        </div>
+
+        <div class="mdl-mega-footer__drop-down-section">
+          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+          <ul class="mdl-mega-footer__link-list">
+            <li><a href="#">買取のポイント</a></li>
+          </ul>
+        </div>
+
+        <div class="mdl-mega-footer__drop-down-section">
+          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+          <ul class="mdl-mega-footer__link-list">
+            <li><a href="#">よくある質問</a></li>
+          </ul>
+        </div>
+
+        <div class="mdl-mega-footer__drop-down-section">
+          <ul class="mdl-mega-footer__link-list">
+            <li><a href="#">買取の流れ</a></li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div class="mdl-mega-footer__bottom-section">
+        <ul class="mdl-mega-footer__link-list">
+          <li><a href="#">店舗情報</a></li>
+        </ul>
+      </div>
+
+    </footer>
+  </div>
 </footer>
