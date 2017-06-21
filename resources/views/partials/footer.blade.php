5993493755e1b83d8514b61e1972d61e74986ce6
千葉工 <chibataku0815@gmail.com>
2017-06-21 23:23:48 +0900

    fix footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,7 +8,8 @@
           <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
           <ul class="mdl-mega-footer__link-list">
             <li><a href="#">運営者情報</a></li>
-            <li><a href="#">店舗情報</a></li>
+            <li><a href="#">買取のポイント</a></li>
+            <li><a href="#">よくある質問</a></li>
 
           </ul>
         </div>
@@ -16,27 +17,18 @@
         <div class="mdl-mega-footer__drop-down-section">
           <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
           <ul class="mdl-mega-footer__link-list">
-            <li><a href="#">買取のポイント</a></li>
+            <li><a href="#">買取の流れ</a></li>
+            <li><a href="#">店舗情報</a></li>
             <li><a>問合せ</a></li>
           </ul>
         </div>
 
-        <div class="mdl-mega-footer__drop-down-section">
-          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
-          <ul class="mdl-mega-footer__link-list">
-            <li><a href="#">よくある質問</a></li>
-          </ul>
-        </div>
 
-        <div class="mdl-mega-footer__drop-down-section">
-          <ul class="mdl-mega-footer__link-list">
-            <li><a href="#">買取の流れ</a></li>
-          </ul>
-        </div>
+
 
       </div>
 
-      <div class="mdl-mega-footer--bottom-section">
+      <div class="mdl-mega-footer--bottom-section flex flex--center">
             <div class="mdl-logo">
               Copyright © 2017 japan-sneaker All Rights Reserved
             </div>
