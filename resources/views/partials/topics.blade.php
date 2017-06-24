920de2e30476f32b0975d9fa95620b18e9fd2ac0
千葉工 <chibataku0815@gmail.com>
2017-06-25 06:52:20 +0900

    fix index topics

--- resources/views/partials/topics.blade.php
+++ resources/views/partials/topics.blade.php
@@ -2,7 +2,7 @@
   <div class="mdl-grid mdl-grid--no-spacing">
     <div class="mdl-cell mdl-cell--12-col flex flex--column-center">
       <img src="{{bloginfo( 'url' )}}/wp-content/uploads/2017/06/top-title-topics.png" alt="新着情報" class="alignnone size-full wp-image-188" />
-        <?php $query = new WP_Query( array('post_type' => 'post', 'category_name'  => 'topics', 'posts_per_page' => 5) ); ?>
+        <?php $query = new WP_Query( array('post_type' => 'post', 'posts_per_page' => 6) ); ?>
         <div class="mdl-cell mdl-cell--12-col mdl-grid">
         <?php if ( $query->have_posts() ) : ?>
             <?php while ( $query->have_posts() ) : $query->the_post(); ?>
