231736458ab6cdffc3c35adaad30ba02168d2793
千葉工 <chibataku0815@gmail.com>
2017-06-26 05:39:52 +0900

    fix category htmltag

--- resources/views/partials/content-cat-parent.blade.php
+++ resources/views/partials/content-cat-parent.blade.php
@@ -2,7 +2,7 @@
     $cat = get_the_category(); $cat = $cat[0];
     echo '<section class="mdl-color--black mdl-color-text--red ' . $cat->category_nicename . '" />';
   @endphp
-  <div class="mdl-grid flex flex--center">
+  <h2 class="mdl-grid flex flex--center">
     <div class="title title--fontMiddle title--bold mdl-cell mdl-cell--4-col flex flex--center">
       @php
           $cats = get_the_category();
@@ -17,15 +17,15 @@
       @endphp
 
     </div>
-    <h2 class="title title--fontMiddle title--bold  mdl-cell mdl-cell--8-col mdl-color-text--white">
+    <div class="title title--fontMiddle title--bold  mdl-cell mdl-cell--8-col mdl-color-text--white">
       を売るなら
       @php
         $upload_dir = wp_upload_dir();
       @endphp
       <img src="{{ $upload_dir['baseurl'] }}/2017/06/logo-white.svg" alt="日本スニーカー買取センター" class="logo logo--small ie-svg">
       へ
-    </h2>
-  </div>
+    </div>
+  </h2>
   </section>
 
 
