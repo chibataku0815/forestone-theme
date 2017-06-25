7c665355979e5f7572a64f3632f07b9f397e7b6f
千葉工 <chibataku0815@gmail.com>
2017-06-26 05:10:20 +0900

    fix head descripion

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,6 +4,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   @php(wp_head())
+  @php
+    $description = get_post_meta($post->ID, _aioseop_description, true);
+    $keywords = get_post_meta($post->ID, _aioseop_keywords, true);
+  @endphp
+
+  @if($description=='')
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+  @endif
+
+  @if($keywords=='')
+    <meta name="keywords" content="nike,買取,買い取り,買取り">
+  @endif
+
   <!--[if lte IE 10 ]>
   <script src="//cdnjs.cloudflare.com/ajax/libs/flexibility/2.0.1/flexibility.js" data-turbolinks-track="true"></script>
   <![endif]-->
