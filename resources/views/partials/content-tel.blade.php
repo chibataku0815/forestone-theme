0ab15086b42b7cffb511016df347ff518ad9bfee
千葉工 <chibataku0815@gmail.com>
2017-07-01 23:32:48 +0900

    fix tel

--- resources/views/partials/content-tel.blade.php
+++ resources/views/partials/content-tel.blade.php
@@ -4,7 +4,7 @@
       <div class="tel__title">お問合せ</div>
       <div class="tel__item">
       <i class="material-icons">perm_phone_msg</i>
-      <span class="mdl-color-text--red">0284-22-7923</span>
+      0284-22-7923
       <div class="mdl-color-text--white">10:00～18:00</div>
       </div>
 
