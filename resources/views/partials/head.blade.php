f863aa5711a2f465a7c57df1166a964f262a320b
千葉工 <chibataku0815@gmail.com>
2017-07-03 19:18:11 +0900

    fix category

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -23,8 +23,22 @@
     @endif
   @endif
 
-  @php(wp_head())
   @if( is_category() )
+    @php
+      remove_action('wp_head', '_wp_render_title_tag', 1)
+    @endphp
+  @endif
+
+  @if( is_category() )
+    @php
+      $cat_id = get_queried_object()->cat_ID; $post_id = 'category_'.$cat_id
+    @endphp
+    @php
+      remove_action('wp_head', '_wp_render_title_tag', 1)
+    @endphp
+
+      <title>{{the_field('meta_title',$post_id)}}</title>
+
     @if($description=='')
       <meta name="description" content="@php(the_title())の高価買取は【日本スニーカー買取センター】加水分解から、黄ばみがあっても買い取ります、まずは一度ご相談ください。2万足の買取実績経験があなたのJORDANに正当価格をお付けします！">
     @endif
@@ -33,11 +47,18 @@
       <meta name="keywords" content="@php(the_title()),nike,買取,買い取り,買取り">
     @endif
   @endif
+
+
+
+  @php(wp_head())
+
   @php
     $title = get_post_meta($post->ID, _aioseop_title, true);
     $description = get_post_meta($post->ID, _aioseop_description, true);
     $keywords = get_post_meta($post->ID, _aioseop_keywords, true);
   @endphp
+
+
 
   <!--[if lte IE 10 ]>
   <script src="//cdnjs.cloudflare.com/ajax/libs/flexibility/2.0.1/flexibility.js" data-turbolinks-track="true"></script>
