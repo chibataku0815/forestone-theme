f1c387c7849e70a2e33f79995ba83c681dd0b976
千葉工 <chibataku0815@gmail.com>
2017-07-03 15:13:22 +0900

    fix topics

--- resources/views/partials/topics.blade.php
+++ resources/views/partials/topics.blade.php
@@ -1,8 +1,9 @@
 <div class="wrapper wrapper--space mdl-color--grey-50">
   <div class="mdl-grid mdl-grid--no-spacing">
+
     <div class="mdl-cell mdl-cell--12-col flex flex--column-center">
       <img src="{{bloginfo( 'url' )}}/wp-content/uploads/2017/06/top-title-topics.png" alt="新着情報" class="alignnone size-full wp-image-188" />
-        <?php $query = new WP_Query( array('post_type' => 'post', 'posts_per_page' => 6) ); ?>
+        <?php $query = new WP_Query( array('post_type' => '新着買取ブログ', 'posts_per_page' => 6) ); ?>
         <div class="mdl-cell mdl-cell--12-col mdl-grid">
         <?php if ( $query->have_posts() ) : ?>
             <?php while ( $query->have_posts() ) : $query->the_post(); ?>
