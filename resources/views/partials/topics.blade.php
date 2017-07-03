581b7308a222f927f4f38d203ccaebd97c94f53f
千葉工 <chibataku0815@gmail.com>
2017-07-03 15:44:59 +0900

    fix topics

--- resources/views/partials/topics.blade.php
+++ resources/views/partials/topics.blade.php
@@ -1,37 +1,31 @@
-<div class="wrapper wrapper--space mdl-color--grey-50">
-  <div class="mdl-grid mdl-grid--no-spacing">
-    <div class="mdl-cell mdl-cell--12-col flex flex--column-center">
-      <img src="{{bloginfo( 'url' )}}/wp-content/uploads/2017/06/top-title-topics.png" alt="新着情報" class="alignnone size-full wp-image-188" />
-        <?php $query = new WP_Query( array('post_type' => 'post', 'posts_per_page' => 6) ); ?>
-        <div class="mdl-cell mdl-cell--12-col mdl-grid">
-        <?php if ( $query->have_posts() ) : ?>
-            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-              <div class="topics mdl-cell mdl-cell--6-col mdl-grid">
-                  <div class="mdl-cell mdl-cell--3-col mdl-cell--8-col-tablet mdl-cell--4-col-phone flex flex--column-left">
-                    <em>{{the_time('Y.m.j')}}</em>
-                  </div>
+<?php $query = new WP_Query( array('post_type' => 'blog', 'posts_per_page' => 6) ); ?>
+  <?php if ( $query->have_posts() ) : ?>
+    <div class="wrapper wrapper--space mdl-color--grey-50">
+      <div class="mdl-grid mdl-grid--no-spacing">
+        <div class="mdl-cell mdl-cell--12-col flex flex--column-center">
+          <img src="{{bloginfo( 'url' )}}/wp-content/uploads/2017/06/top-title-topics.png" alt="新着情報" class="alignnone size-full wp-image-188" />
+            <div class="mdl-cell mdl-cell--12-col mdl-grid">
+      <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+        <div class="topics mdl-cell mdl-cell--6-col mdl-grid">
+            <div class="mdl-cell mdl-cell--3-col mdl-cell--8-col-tablet mdl-cell--4-col-phone flex flex--column-left">
+              <em>{{the_time('Y.m.j')}}</em>
+            </div>
 
-                  <div class="mdl-cell mdl-cell--6-col flex flex--column-left">
-                    <a href="<?php the_permalink() ?>"> {{the_title()}}</a>
-                  </div>
-                  <div class="mdl-cell mdl-cell--3-col mdl-cell--8-col-tablet mdl-cell--4-col-phone flex flex--center">
-                    @if(has_post_thumbnail())
-                        {{the_post_thumbnail() }}
-                    @else
-                        <img src="{{bloginfo( 'url' )}}/wp-content/uploads/2017/06/IMG_7576.jpg"  class="topics__image" />
-                    @endif
-                </div>
-                </div>
-                <?php endwhile; ?>
-            <?php else : ?>
-                <div class="mdl-cell mdl-cell--12-col flex flex--center">
-                  <?php _e('Sorry, no posts matched your criteria.') ?>
-                </div>
+            <div class="mdl-cell mdl-cell--6-col flex flex--column-left">
+              <a href="<?php the_permalink() ?>"> {{the_title()}}</a>
+            </div>
+            <div class="mdl-cell mdl-cell--3-col mdl-cell--8-col-tablet mdl-cell--4-col-phone flex flex--center">
+              @if(has_post_thumbnail())
+                  {{the_post_thumbnail() }}
+              @else
+                  <img src="{{bloginfo( 'url' )}}/wp-content/uploads/2017/06/IMG_7576.jpg"  class="topics__image" />
+              @endif
           </div>
-        <?php endif; wp_reset_postdata(); ?>
-        </div>
-
-
+          </div>
+          <?php endwhile; ?>
+          </div>
+      </div>
     </div>
   </div>
-</div>
+  <?php else : ?>
+<?php endif; wp_reset_postdata(); ?>
