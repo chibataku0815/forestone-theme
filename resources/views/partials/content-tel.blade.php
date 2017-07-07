78a738f8293576e160bd05663270ed4f3edcb841
千葉工 <chibataku0815@gmail.com>
2017-07-07 18:54:04 +0900

    fix phone number

--- resources/views/partials/content-tel.blade.php
+++ resources/views/partials/content-tel.blade.php
@@ -4,7 +4,7 @@
       <div class="tel__title">お問合せ</div>
       <div class="tel__item">
       <i class="material-icons">perm_phone_msg</i>
-      0284-22-7923
+      0120-955-161
       <div class="mdl-color-text--white">10:00～18:00</div>
       </div>
 
